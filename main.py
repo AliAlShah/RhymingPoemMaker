43501f2ffeff33a1baaf96eebc4f5c95f5cdaf68
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-05-17 19:41:29 +0300

    Update main.py
    Added infinite loop which breaks when input is equal to "no"

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ def make_poem(start_word, suitable_words):
         suitable_words = " ".join(suitable_words)
         print(f"{start_word} {suitable_words}")
 
-all_words = [word for word in words.split()]
-input_word = input("Enter a refrence word...   ")
-suitable_words = [suitable_word for suitable_word in all_words if suitable_word[-3:] == input_word[-3:]]
-
-make_poem(input_word, suitable_words)
-
-
 while True:
-    if input("Do you want to regenirate the poem? (Y/N)   ").lower() == "y":
+    all_words = [word for word in words.split()]
+    input_word = input("Enter a refrence word...   ")
+    suitable_words = [suitable_word for suitable_word in all_words if suitable_word[-3:] == input_word[-3:]]
+
+    if input_word != "no":
         make_poem(input_word, suitable_words)
     else:
         break
+
+
+    if input("Do you want to regenirate the poem? (Y/N)   ").lower() == "y":
+        make_poem(input_word, suitable_words)
+    else:
+        pass
